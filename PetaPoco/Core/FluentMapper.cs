2be619e9b85be1c44a3cb66342cd54ae92dfdc1e
Luuk Sommers <luuk.sommers@gmail.com>
2013-07-13 12:58:10 +0200

    Fixed namespace

--- PetaPoco/Core/FluentMapper.cs
+++ PetaPoco/Core/FluentMapper.cs
@@ -5,7 +5,7 @@ using System.Linq.Expressions;
 using System.Reflection;
 using System.Text;
 
-namespace PetaPoco.Core
+namespace PetaPoco
 {
 	public class FluentColumnMap
 	{
--- PetaPoco/PetaPoco.cs
+++ PetaPoco/PetaPoco.cs
@@ -2515,6 +2515,78 @@ namespace PetaPoco
 		}
 	}
 
+	public class FluentColumnMap
+	{
+		public ColumnInfo ColumnInfo { get; set; }
+		public Func<object, object> FromDbConverter { get; set; }
+		public Func<object, object> ToDbConverter { get; set; }
+
+		public FluentColumnMap() { }
+		public FluentColumnMap(ColumnInfo columnInfo) : this(columnInfo, null) { }
+		public FluentColumnMap(ColumnInfo columnInfo, Func<object, object> fromDbConverter) : this(columnInfo, fromDbConverter, null) { }
+		public FluentColumnMap(ColumnInfo columnInfo, Func<object, object> fromDbConverter, Func<object, object> toDbConverter)
+		{
+			ColumnInfo = columnInfo;
+			FromDbConverter = fromDbConverter;
+			ToDbConverter = toDbConverter;
+		}
+	}
+
+	public abstract class FluentMapper<T> : IMapper
+	{
+		public Dictionary<string, FluentColumnMap> Mappings = new Dictionary<string, FluentColumnMap>();
+		public TableInfo TableInfo = new TableInfo();
+
+		protected FluentMapper(string tableName, string primaryKey)
+		{
+			TableInfo.TableName = tableName;
+			TableInfo.PrimaryKey = primaryKey;
+		}
+
+		public TableInfo GetTableInfo(Type pocoType)
+		{
+			return TableInfo;
+		}
+
+		public ColumnInfo GetColumnInfo(PropertyInfo pocoProperty)
+		{
+			return Mappings[pocoProperty.Name].ColumnInfo;
+		}
+
+		public Func<object, object> GetFromDbConverter(PropertyInfo TargetProperty, Type SourceType)
+		{
+			return Mappings[TargetProperty.Name].FromDbConverter;
+		}
+
+		public Func<object, object> GetToDbConverter(PropertyInfo SourceProperty)
+		{
+			return Mappings[SourceProperty.Name].FromDbConverter;
+		}
+	}
+
+	public static class FluentMapperExtensions
+	{
+		public static FluentMapper<T> Property<T, P>(this FluentMapper<T> obj, Expression<Func<T, P>> action, string column) where T : class
+		{
+			return obj.Property(action, column, null);
+		}
+
+		public static FluentMapper<T> Property<T, P>(this FluentMapper<T> obj, Expression<Func<T, P>> action, string column, Func<object, object> fromDbConverter) where T : class
+		{
+			return obj.Property(action,  column, fromDbConverter, null);
+		}
+
+		public static FluentMapper<T> Property<T, P>(this FluentMapper<T> obj, Expression<Func<T, P>> action, string column, Func<object, object> fromDbConverter, Func<object, object> toDbConverter) where T : class
+		{
+			var expression = (MemberExpression)action.Body;
+			string name = expression.Member.Name;
+
+			obj.Mappings.Add(name, new FluentColumnMap(new ColumnInfo() { ColumnName = column }, fromDbConverter, toDbConverter));
+
+			return obj;
+		}
+	}
+
 	/// <summary>
 	/// IMapper provides a way to hook into PetaPoco's Database to POCO mapping mechanism to either
 	/// customize or completely replace it.
@@ -4154,75 +4226,6 @@ namespace PetaPoco
 			public static T Instance = new T();
 		}
 
-	}
-
-	namespace Core
-	{
-		public class FluentMap
-		{
-			public ColumnInfo ColumnInfo { get; set; }
-			public Func<object, object> FromDbConverter { get; set; }
-			public Func<object, object> ToDbConverter { get; set; }
-
-			public FluentMap() { }
-			public FluentMap(ColumnInfo columnInfo) : this(columnInfo, null) { }
-			public FluentMap(ColumnInfo columnInfo, Func<object, object> fromDbConverter) : this(columnInfo, fromDbConverter, null) { }
-			public FluentMap(ColumnInfo columnInfo, Func<object, object> fromDbConverter, Func<object, object> toDbConverter)
-			{
-				ColumnInfo = columnInfo;
-				FromDbConverter = fromDbConverter;
-				ToDbConverter = toDbConverter;
-			}
-		}
-
-		public class FluentMapper<T> : IMapper
-		{
-			public Dictionary<string, FluentMap> Mappings = new Dictionary<string, FluentMap>();
-			private static TableInfo _ti;
-
-			public TableInfo GetTableInfo(Type pocoType)
-			{
-				return _ti;
-			}
-
-			public ColumnInfo GetColumnInfo(PropertyInfo pocoProperty)
-			{
-				return Mappings[pocoProperty.Name].ColumnInfo;
-			}
-
-			public Func<object, object> GetFromDbConverter(PropertyInfo TargetProperty, Type SourceType)
-			{
-				return Mappings[TargetProperty.Name].FromDbConverter;
-			}
-
-			public Func<object, object> GetToDbConverter(PropertyInfo SourceProperty)
-			{
-				return Mappings[SourceProperty.Name].FromDbConverter;
-			}
-		}
-
-		public static class FluentMapperExtensions
-		{
-			public static FluentMapper<T> Property<T, P>(this FluentMapper<T> obj, Expression<Func<T, P>> action, string column) where T : class
-			{
-				return obj.Property(action, column, null);
-			}
-
-			public static FluentMapper<T> Property<T, P>(this FluentMapper<T> obj, Expression<Func<T, P>> action, string column, Func<object, object> fromDbConverter) where T : class
-			{
-				return obj.Property(action,  column, fromDbConverter, null);
-			}
-
-			public static FluentMapper<T> Property<T, P>(this FluentMapper<T> obj, Expression<Func<T, P>> action, string column, Func<object, object> fromDbConverter, Func<object, object> toDbConverter) where T : class
-			{
-				var expression = (MemberExpression)action.Body;
-				string name = expression.Member.Name;
-
-				obj.Mappings.Add(name, new FluentMap(new ColumnInfo() { ColumnName = column }, fromDbConverter, toDbConverter));
-
-				return obj;
-			}
-		}
 	}
 
 	namespace DatabaseTypes
